1d72ef08ed6c6200c1e07dd28c9dec2255b4501d
yurcik45nova <yurcik45work@gmail.com>
2026-02-10 01:23:44 +0200

    Container components 6

--- src/patterns/ContainerComponents/ContainerComponentsImplementation.jsx
+++ src/patterns/ContainerComponents/ContainerComponentsImplementation.jsx
@@ -7,6 +7,12 @@ const getDataFromServer = async (url) => {
     const response = await axiosInstance.get(url)
     return response.data
 }
+ 
+const getDataFromLocalStorage = key => () => {
+    return localStorage.getItem(key)
+}
+
+const Message = ({msg}) => <h1>{msg}</h1>
 
 export const ContainerComponentsImplementations = () => {
     return (
@@ -14,8 +20,14 @@ export const ContainerComponentsImplementations = () => {
             <DataSourceWithRender
                 getData={() => getDataFromServer("/users/3")}
                 render={(resource) => <UserInfo user={resource} />}
-            >
-            </DataSourceWithRender>
+            />
+
+            <DataSource
+                getData={() => getDataFromLocalStorage("test")}
+                resourceName={"msg"}
+                >
+                    <Message />
+            </DataSource>
         </>
     )
 }
