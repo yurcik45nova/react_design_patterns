4b1c8ba304703133dfa0c6a11062fd108cf0da42
yurcik45nova <yurcik45work@gmail.com>
2026-02-10 03:44:18 +0200

    Controlled flows

--- src/patterns/ControlledAndUncontrolledComponents/ContUncontComponentsImplementation.jsx
+++ src/patterns/ControlledAndUncontrolledComponents/ContUncontComponentsImplementation.jsx
@@ -1,10 +1,11 @@
-import { UncontrolledFlow } from "./uncontrolled-flow";
+import { useState } from "react";
+import { ControlledFlow } from "./controlled-flow";
 
 const StepOne = ({ goNext }) => {
   return (
     <>
       <h1>Step #1 Input your Name</h1>
-      <button onClick={() => goNext({ name: "My Name"})}>Next</button>
+      <button onClick={() => goNext({ name: "My Name" })}>Next</button>
     </>
   );
 };
@@ -13,7 +14,7 @@ const StepTwo = ({ goNext }) => {
   return (
     <>
       <h1>Step #2 Input you age</h1>
-      <button onClick={() => goNext({ name: 23})}>Next</button>
+      <button onClick={() => goNext({ age: 27 })}>Next</button>
     </>
   );
 };
@@ -21,22 +22,41 @@ const StepTwo = ({ goNext }) => {
 const StepThree = ({ goNext }) => {
   return (
     <>
-      <h1>Step #3 Input your country</h1>
-      <button onClick={() => goNext({ name: "Mars"})}>Next</button>
+      <h1>Congredulatons! You qualify for the gift!</h1>
+      <button onClick={() => goNext({})}>Next</button>
+    </>
+  );
+};
+
+const StepFour = ({ goNext }) => {
+  return (
+    <>
+      <h1>Step #4 Input your Country</h1>
+      <button onClick={() => goNext({ country: "Mars" })}>Next</button>
     </>
   );
 };
 
 export const ContUncontComponentsImplementation = () => {
+  const [data, setData] = useState({});
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  const goNext = (dataFromStep) => {
+
+    setData({ ...data, ...dataFromStep })
+    setCurrentStepIndex(currentStepIndex + 1)
+  };
   return (
     <>
-      <UncontrolledFlow onDone={(data) => {
-        console.log("final step data: ", data)
-      }}>
+      <ControlledFlow
+        currentIndex={currentStepIndex}
+        onNext={goNext}
+      >
         <StepOne />
         <StepTwo />
-        <StepThree />
-      </UncontrolledFlow>
+        {data.age > 25 && <StepThree />}
+        <StepFour />
+      </ControlledFlow>
     </>
   );
 };
---
+++ src/patterns/ControlledAndUncontrolledComponents/controlled-flow.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const ControlledFlow = ({ children, onDone, currentIndex, onNext }) => {
+
+  const goNext = (dataFromStep) => {
+    onNext(dataFromStep)
+  }
+
+  const currentChild = React.Children.toArray(children)[currentIndex];
+
+  if (React.isValidElement(currentChild)) {
+    return React.cloneElement(currentChild, { goNext });
+  }
+
+  return currentChild;
+};
