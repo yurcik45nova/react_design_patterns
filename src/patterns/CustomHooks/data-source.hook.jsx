0bf2b4b6d692025e32f0ed791529c354844c1e2c
yurcik45nova <yurcik45work@gmail.com>
2026-02-11 03:29:34 +0200

    CustomHook components 4

---
+++ src/patterns/CustomHooks/data-source.hook.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+export const useDataResource = (getData) => {
+  const [resource, setResource] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getData();
+      setResource(data);
+    })();
+  }, [getData]);
+
+  return resource;
+};
--- src/patterns/CustomHooks/user-info.jsx
+++ src/patterns/CustomHooks/user-info.jsx
@@ -1,13 +1,26 @@
-import { useResource } from "./resource.hook";
-import { useUser } from "./user.hook";
+import { useCallback } from "react";
+import { axiosInstance } from "../../data/axiosInstance";
+import { useDataResource } from "./data-source.hook";
+
+const fetchFromServer = (resourceUrl) => async () => {
+    const response = await axiosInstance.get(resourceUrl);
+    return response.data;
+}
+
+const fetchFromLocalStorage = (key) => () => {
+    const value = localStorage.getItem(key);
+    return value;
+}
 
 export const UserInfo = ({userId}) => {
-    // const user = useUser(userId)
-    const user = useResource(`/users/${userId}`)
+    const fetchUser = useCallback(fetchFromServer(`/users/${userId}`), [userId])
+    const user = useDataResource(fetchUser)
+    const message = useDataResource(fetchFromLocalStorage("test"))
     const { name, age, country, books } = user || {};
 
     return user ? (
         <>
+            <p>{message || "no message"}</p>
             <h2>{name}</h2>
             <p>Age: {age}</p>
             <p>Country: {country}</p>
